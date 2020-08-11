d7950f3ee9fa084d4600b4741461f3fe94cd888d
Pavel Naumov <naumov@it-bproject.ru>
2020-08-11 11:27:13 +0300

    refactor: adjust thumbnail offsets

--- src/utils/calculateThumbnailsContainerDimension.js
+++ src/utils/calculateThumbnailsContainerDimension.js
@@ -1,6 +1,6 @@
-import { THUMBNAIL_WIDTH, THUMBNAIL_OFFSET } from '../constants'
+import { THUMBNAIL_WIDTH } from '../constants'
 
 export default function calculateThumbnailsContainerDimension(total) {
-  const thumbnailOffset = window.innerWidth < 1920 ? 15 : THUMBNAIL_OFFSET
-  return THUMBNAIL_WIDTH * total + (thumbnailOffset * total - thumbnailOffset)
+  let THUMBNAIL_OFFSET = window.innerWidth < 1920 ? 18 : 15
+  return THUMBNAIL_WIDTH * total + (THUMBNAIL_OFFSET * total - THUMBNAIL_OFFSET)
 }
