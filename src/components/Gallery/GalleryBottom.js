6cb24659bc190f6176924c007a725099955769f1
Pavel Naumov <naumov@it-bproject.ru>
2021-04-23 10:59:31 +0300

    fix: gold theme amendments

--- src/components/Gallery/GalleryBottom.js
+++ src/components/Gallery/GalleryBottom.js
@@ -14,10 +14,12 @@ const GalleryBottom = ({
   onRotate,
   toFavourites,
   isFavourite,
+  forwardOverlay,
+  forwardOverlayVisible,
   isGold
 }) => {
   const mediaItem = photos[currentIndex]
-  const { caption, subcaption, fileId } = mediaItem
+  const { caption, subcaption, fileId, avatar } = mediaItem
 
   const _onDownload = () => {
     onDownload(fileId)
@@ -26,7 +28,7 @@ const GalleryBottom = ({
   return (
     <div className='gallery-bottom__inner'>
       <div className='bottom-inner__avatar-container'>
-        <div className='bottom-inner__avatar'>{avatarElement}</div>
+        <div className='bottom-inner__avatar'>{avatar}</div>
         <div className='bottom-inner__name-data'>
           <div className='bottom-inner__name'>
             <>{caption}</>
@@ -58,6 +60,7 @@ const GalleryBottom = ({
           />
         </div>
       </div>
+      <>{forwardOverlayVisible && forwardOverlay}</>
     </div>
   )
 }
